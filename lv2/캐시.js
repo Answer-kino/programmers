e66c257ab943dc52fd3948902a3b93a8e566c891
kino <kino@ianswer.co.kr>
2022-07-21 01:46:54 +0900

    캐시 교체 알고리즘 LRU (Least Recently Used)

---
+++ lv2/캐시.js
@@ -0,0 +1,27 @@
+// 캐시 교체 알고리즘 LRU (Least Recently Used)
+function solution(cacheSize, cities) {
+  var answer = 0
+  let cache = []
+  //캐시 크기가 0인 경우는 따로 처리
+  if (cacheSize === 0) return cities.length * 5
+
+  while (cities.length) {
+    const city = cities.shift().toLowerCase()
+
+    if (cache.includes(city)) {
+      cache.splice(cache.indexOf(city), 1)
+      cache.push(city)
+      answer += 1
+    } else {
+      if (cache.length === cacheSize) {
+        cache.shift()
+      }
+      cache.push(city)
+      answer += 5
+    }
+  }
+  return answer
+}
+
+console.log(solution(3, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
+console.log(solution(3, ["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]))
