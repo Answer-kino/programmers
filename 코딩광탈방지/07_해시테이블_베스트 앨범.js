c8f42c93d996097aab263d23177699c1a652aa61
kino <kino@ianswer.co.kr>
2022-07-30 02:31:32 +0900

    HashTable

---
+++ 코딩광탈방지/07_해시테이블_베스트 앨범.js
@@ -0,0 +1,20 @@
+// HashTable
+function solution(genres, plays) {
+  const genreMap = new Map()
+
+  genres
+    .map((genre, index) => [genre, plays[index]])
+    .forEach(([genre, play], index) => {
+      // genreMap이 undefined일 때의 초기값 설정
+      const data = genreMap.get(genre) || { total: 0, songs: [] }
+      genreMap.set(genre, {
+        total: data.total + play,
+        songs: [...data.songs, { play, index }].sort((a, b) => b.play - a.play).slice(0, 2),
+      })
+    })
+
+  return [...genreMap.entries()]
+    .sort((a, b) => b[1].total - a[1].total)
+    .flatMap((item) => item[1].songs)
+    .map((song) => song.index)
+}
